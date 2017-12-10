0370a4e784c6dbc9d58895de4a9433fc56680f27
NPGroup <noodpgroup@gmail.com>
2017-12-10 20:54:21 +0200

    Update

--- ChatClient/src/com/javabegin/chat/client/ClientWindow.java
+++ ChatClient/src/com/javabegin/chat/client/ClientWindow.java
@@ -1,12 +1,49 @@
 package com.javabegin.chat.client;
 
-public class ClientWindow {
+import com.javabegin.chat.network.TCPConnection;
+import com.javabegin.chat.network.TCPConnectionListener;
 
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.io.IOException;
+
+
+public class ClientWindow implements TCPConnectionListener {
+
+        private static final String IP_ADDR = "192.168.1.14";
+        private static final int PORT = 8189;
+        private TCPConnection connection;
+
+
+    private ClientWindow() {
+        Scanner nameInput = new Scanner(System.in);
+        try {
+            connection = new TCPConnection(this, IP_ADDR, PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String name = nameInput.nextLine();
+
+
 
     }
 
-    private ClientWindow() {
+    public void onConnectionReady(com.javabegin.chat.network.TCPConnection tcpConnection) {
+
+    }
+
+    public void onReceiveString(com.javabegin.chat.network.TCPConnection tcpConnection, String value) {
+
+    }
+
+    public void onDisconnect(com.javabegin.chat.network.TCPConnection tcpConnection) {
+
+    }
+
+    public void onExeption(com.javabegin.chat.network.TCPConnection tcpConnection, Exception e) {
+
+    }
+
+    private synchronized void printMag(String msg) {
 
     }
 }
