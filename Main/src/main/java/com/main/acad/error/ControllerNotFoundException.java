06b4d71e40096525069ee5aa265e9b3be06768e0
sasha96 <sasha80630645484@ukr.net>
2018-01-23 17:05:44 +0200

    gitignore

--- Main/src/main/java/com/main/acad/error/ControllerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.main.acad.error;
-
-public class ControllerNotFoundException extends RuntimeException {
-    public ControllerNotFoundException(String message) {
-        super(message);
-    }
-}
--- Main/src/main/java/com/main/acad/serializator/JsonSerializer.java
+++
@@ -1,7 +0,0 @@
-package com.main.acad.serializator;
-
-public interface JsonSerializer {
-    String write(Object obj) throws IllegalAccessException;
-
-    Object read(String string, Class clazz, Class objClass) throws Exception;
-}
